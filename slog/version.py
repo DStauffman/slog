075c7384660dcc5d760c10c1a94feb938face700
David Stauffer <david.c.stauffer@lmco.com>
2026-06-22 12:52:34 -0700

    Remove isort as dependency and use ruff instead.

--- slog/version.py
+++ slog/version.py
@@ -8,7 +8,7 @@ Notes
 """
 
 # %% Constants
-version_info = (1, 5, 1)
+version_info = (1, 5, 2)
 
 # Below is data about the minor release history for potential use in deprecating older support.
 # For inspiration, see: https://numpy.org/neps/nep-0029-deprecation_policy.html
